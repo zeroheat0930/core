9b0a9dda134671cfe12c8181089489fb6779b279
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-10-26 15:18:34 +0900

    의존관계 자동주입 -의존관계 주입은 크게 4가지 방법이 있다.  생성자 주입 -- final로 불변,필수 의존관계 사용  수정자 주입(setter 주입) -- 선택,변경 가능성 의존관계 사용  필드 주입 -- 코드가 간결해지지만 외부에서 변경불가하여 테스트 하기 힘듬  일반 메서드 주입
     생성자, 수정자는 써도됨
    
     필드 주입, 일반 메서드는 왠만하면 버리자.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,15 +4,32 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
     //private final DiscountPolicy discountPolicy = new FIxDiscountPolicy();
-    private final DiscountPolicy discountPolicy;
+    private DiscountPolicy discountPolicy;
 
+    //수정자 주입(setter 주입
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    //생성자 주입
+    //new OrderServiceImpl(memberRepository,discountPolicy);
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
