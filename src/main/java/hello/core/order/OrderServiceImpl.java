658ccf2c96c50793e8321aeef0291c0866e7cf82
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-10-27 19:03:43 +0900

    의존관계 자동주입 -조회 빈이 2개 이상 - 문제  생성자 주입일떄 방식은 3가지 있는데 (조회 대상 빈이 2개 이상일 때 해결 방법  @Autowired 필드 명 매칭  @Qualifier @Qualifier끼리 매칭 빈 이름 매칭  @Primary 사용)
    그중에 qualifier쓸때 애노테이션 직접 만들기
    @Qualifier("mainDiscountPolicy") 이렇게 문자를 적으면컴파일시 타입 체크가 안된다. 다음과 같은 애노테이션을 만들어서 문제를 해결할 수 있다

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -8,34 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
-//@Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
-
-    //private final DiscountPolicy discountPolicy = new FIxDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
-    //수정자 주입(setter 주입
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//    }
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    //생성자 주입
-    //new OrderServiceImpl(memberRepository,discountPolicy);
-
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- src/test/java/hello/core/order/orderServiceImplTest.java
+++ src/test/java/hello/core/order/orderServiceImplTest.java
@@ -4,13 +4,11 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-class OrderServiceImplTest {
+class orderServiceImplTest {
 
     @Test
     void createOrder() {
