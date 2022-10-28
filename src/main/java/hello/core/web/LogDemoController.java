4ed4917cf2e6ce69b113be7441a932f1919885dc
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-10-29 00:13:32 +0900

    Bean 스코프 - proxyMode로 문제해결
    1. 적용 대상이 인터페이스가 아닌 클래스면 TARGET_CLASS 를 선택
    2. 적용 대상이 인터페이스면 INTERFACES 를 선택
    
    이렇게 하면 MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request와 상관 없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있다

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -16,13 +16,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
         myLogger.setRequestURL(requestURL);
 
 
